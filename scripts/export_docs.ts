ec0b809df11c71f8d10f9467c2235713c90fde95
Andre Meyer <andre@enso.finance>
2023-07-14 16:21:00 +0200

    add docs generator

---
+++ scripts/export_docs.ts
@@ -0,0 +1,34 @@
+import fg from 'fast-glob';
+import matter from 'gray-matter';
+import fs from 'node:fs';
+import path from 'node:path';
+
+const rootDir = path.join(__dirname, '..');
+const docsDir = path.join(rootDir, 'docs');
+
+(async () => {
+  // Initialize output dir
+  fs.mkdirSync(docsDir);
+
+  // Find all mdx files
+  const mdxFiles = await fg('hooks/**/*.mdx', {
+    cwd: rootDir,
+    deep: 10,
+  });
+
+  // Read frontmatter
+  const docs: string[] = [];
+
+  mdxFiles.map((mdxFile) => {
+    const mdxContent = fs.readFileSync(path.join(rootDir, mdxFile), { encoding: 'utf8' });
+    const parsed = matter(mdxContent);
+    const frontmatter = parsed.data;
+
+    const fileName = `${frontmatter.name}.mdx`;
+
+    docs.push(frontmatter.name);
+    fs.writeFileSync(path.join(docsDir, fileName), mdxContent);
+  });
+
+  fs.writeFileSync(path.join(docsDir, 'routes.json'), JSON.stringify(docs, null, 2));
+})();
