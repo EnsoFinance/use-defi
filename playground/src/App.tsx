be841fd9632369d8507e04754b302447d12fde1b
Andre Meyer <andre@enso.finance>
2023-07-14 15:23:26 +0200

    more boilerplate stuff

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,18 +1,22 @@
-import './App.css'
+import { usePositions } from 'use-defi';
 
-import { usePositions } from "use-defi"
+import './App.css';
 
 function App() {
-  const hookResult = usePositions()
+  const hookResult = usePositions();
 
   return (
     <>
       <div>
-        <h1><code>use-defi</code><br />React Hooks Playground</h1>
+        <h1>
+          <code>use-defi</code>
+          <br />
+          React Hooks Playground
+        </h1>
         {hookResult}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
