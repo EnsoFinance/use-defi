96da1eae65afbb84663d08c50a9052860e129d5d
Andre Meyer <andre@enso.finance>
2023-07-24 14:46:51 +0200

    fix build, add type definition files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,19 +4,38 @@ import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 import includePaths from 'rollup-plugin-includepaths';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: `dist/index.js`,
-      format: 'cjs',
-      exports: 'default',
-    },
-    {
-      file: `dist/index.esm.js`,
-      format: 'esm',
-    },
-  ],
-  plugins: [resolve(), esbuild(), commonjs(), dts(), includePaths({ paths: './src' })],
-  external: ['react'],
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: `dist/index.cjs.js`,
+        format: 'cjs',
+      },
+      {
+        file: `dist/index.esm.js`,
+        format: 'esm',
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      includePaths({ paths: './src' }),
+      esbuild({
+        jsx: 'transform', // default, or 'preserve'
+        jsxFactory: 'React.createElement',
+        jsxFragment: 'React.Fragment',
+      }),
+    ],
+    external: ['react'],
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.d.ts',
+      },
+    ],
+    plugins: [resolve(), commonjs(), includePaths({ paths: './src' }), dts()],
+  },
+];
