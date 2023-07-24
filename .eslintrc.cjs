8504145c0c8344f3839f5edddc02df34317ee545
Andre Meyer <andre@enso.finance>
2023-07-24 09:53:26 +0200

    fix ignore scripts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,9 +21,6 @@ module.exports = {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-explicit-any': ['warn'],
-    '@typescript-eslint/no-unsafe-member-access': ['warn'],
     '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/no-unsafe-argument': 'off',
   },
 };
