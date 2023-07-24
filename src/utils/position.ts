14a3b9ff30769055a5232427482d77ca9e6a2bc5
Andre Meyer <andre@enso.finance>
2023-07-24 09:38:26 +0200

    fix lint issue?

--- src/utils/position.ts
+++ src/utils/position.ts
@@ -1,6 +1,6 @@
 import { BasePosition, PoolPosition, Position } from 'types/api';
 
-export const getTokenAddressFromPosition = (position: Position) => {
+export const getTokenAddressFromPosition = (position: Position): string => {
   if ((position as BasePosition).address) return (position as BasePosition).address;
 
   return (position as PoolPosition).tokenAddress;
