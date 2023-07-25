12c8d1dac0f6eb89bf9da25f88230eb5b26d9f79
Andre Meyer <andre@enso.finance>
2023-07-25 14:29:09 +0200

    fix broken api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const USE_POSITIONS_DATA_SOURCE = 'https://enso-scrape.s3.us-east-2.amazonaws.com/output/backend/positions.json';
 
-export const ENSO_API = process.env.ENSO_API ?? 'http://api.enso.finance';
+export const ENSO_API = process.env.ENSO_API ? process.env.ENSO_API : 'http://api.enso.finance';
 
 export const NATIVE_TOKEN_ALIAS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
